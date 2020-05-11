a7d50a18405a2b62c01125e321d9d52e3e7a6e0c
ShahinGH <ghaneishahin73@gmail.com>
2020-05-11 17:27:32 +0200

    cv updated

--- src/components/topNavigation.js
+++ src/components/topNavigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import * as moment from "moment";
 import { Nav, Navbar, Button, NavDropdown, Dropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,7 +9,6 @@ import {
   faGithub,
   faLinkedin
 } from "@fortawesome/free-brands-svg-icons";
-import { Link } from "react-router-dom";
 
 const TopNavigation = () => {
   const currentDate = moment().format("MMMM Do YYYY");
